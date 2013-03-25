ef0b8702e22210051a99fec6cfe18419db2de435
Michael Millian <xenakaaii@gmail.com>
2013-03-24 22:01:11 -0400

    Test Commit 5

--- Crypto/src/Caesar.java
+++ Crypto/src/Caesar.java
@@ -3,11 +3,11 @@ public class Caesar {
 
 	private char[] ciphertext;
 	
-	public void Caesar(char[] ciphertext){
+	public Caesar(char[] ciphertext){
 		setCiphertext(ciphertext);
 	}
 	
-	private char[] decrypt(int key){
+	public char[] decrypt(int key){
 		char[] cleartext = this.getCiphertext();
 		return cleartext;
 	}
