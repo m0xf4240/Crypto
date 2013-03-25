3d54c72d1bdc3c563362385f70fa255983a7af0b
Michael Millian <xenakaaii@gmail.com>
2013-03-24 22:38:57 -0400

    Added bruteForce() to Caesar/out of Driver

--- Crypto/src/Caesar.java
+++ Crypto/src/Caesar.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+
 
 public class Caesar {
 
@@ -9,7 +11,20 @@ public class Caesar {
 		this.setSpaceSize(size);
 	}
 	
-	public char[] decrypt(int key){
+	public void bruteForce() throws IOException{
+		for (int key=0; key<this.getSpaceSize(); key++){
+			char[] cleartext = this.decrypt(key);
+			for (int i=0; i<cleartext.length; i++){
+				System.out.print(cleartext[i]);
+			}
+			System.out.println("\n");
+			
+			//Press enter to continue
+			System.in.read();
+		}
+	}
+	
+	private char[] decrypt(int key){
 		char [] cleartext = new char[this.getCiphertext().length];
 		for (int i = 0; i < cleartext.length; i = (i + 1)) {
 			cleartext[i] = (char)((this.getSpaceSize() + (this.getCiphertext()[i] - key)) % this.getSpaceSize());
--- Crypto/src/Driver.java
+++ Crypto/src/Driver.java
@@ -15,17 +15,7 @@ public class Driver {
 		int size=255;
 		
 		Caesar c = new Caesar(ciphertext, size);
-		
-		for (int key=0; key<256; key++){
-			char[] cleartext = c.decrypt(key);
-			for (int i=0; i<cleartext.length; i++){
-				System.out.print(cleartext[i]);
-			}
-			System.out.println("\n");
-			
-			//Press enter to continue
-			System.in.read();
-		}
+		c.bruteForce();
 	}
 	
 }
